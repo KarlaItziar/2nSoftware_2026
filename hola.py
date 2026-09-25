36765ec383265c0fdcb6060492fde5ea7ee5b128
KarlaItziar <1800444@uab.cat>
2026-09-25 09:21:55 +0200

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,1 +1,18 @@
-print("Hola, Mundo!")
+###
+# Exercicis - print()
+# Completa els exercicis següents utilitzant la funció print()
+###
+
+# Exercici 1
+# Imprimeix el teu nom i cognom a la consola
+print("Karla Suquia Vegué")
+
+# Exercici 2
+# Imprimeix les paraules "Python", "és", "divertit" separades per un guió (-)
+# Pista: utilitza el paràmetre 'sep' de print()
+print("Python", "és", "divertit", sep="-")
+
+
+# Exercici 3
+# Imprimeix el resultat de la suma 15 + 27
+print(15 + 27)
